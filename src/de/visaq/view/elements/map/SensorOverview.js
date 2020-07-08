349d4e1fd517785066334425cae2291dce39f4eb
Katharina Biernacka <kath.biernacka@gmail.com>
2020-07-08 17:23:55 +0200

    added files used in class diagram

---
+++ src/de/visaq/view/elements/map/SensorOverview.js
@@ -0,0 +1,8 @@
+import React from 'react';
+export default class Sidebar extends React.Component {
+  render() {
+    return (
+        <SideNav></SideNav>
+    );
+  }
+}
---
+++ src/de/visaq/view/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import {Navbar}  from 'src/de/visaq/view/elements/navbar';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {I18nextProvider} from "react-i18next";
+import i18next from "i18next";
+import common_de from "vis/translations_de.json";
+import common_en from "vis/translations_en.json";
+
+{/* Initializies the language*/}
+    i18next.init({
+        interpolation: { escapeValue: false },  // React already does escaping
+        lng: 'de',                              // language to use
+        resources: {
+            en: {
+                common: common_en               // 'common' is our custom namespace
+            },
+            de: {
+                common: common_de
+            },
+        },
+    });
+
+class Main extends React.Component {
+    render() {
+      return (
+        <ThemeContext.Provider value="dark">
+        <Navbar></Navbar>
+      </ThemeContext.Provider>
+      );
+    }
+  }
+  
+  // ========================================
+      {/* Needed for the Language*/}
+ReactDOM.render(
+  <React.StrictMode>
+      <I18nextProvider i18n={i18next}>
+          <App/>
+      </I18nextProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
+  
