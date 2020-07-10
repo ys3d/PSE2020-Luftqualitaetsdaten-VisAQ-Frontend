3ce3c8f43e1a87a652b3c66dba47555734329974
Maria Kraus <maria.kraus1@web.de>
2020-07-10 12:14:55 +0200

    Added Map to mapView

--- src/de/visaq/view/MapView.js
+++ src/de/visaq/view/MapView.js
@@ -5,13 +5,29 @@ import { render } from 'react-dom'
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 import "./MapView.css";
 
-export default function MapView() {
-  return (
-    <Map center={[45.4, -75.7]} zoom={12}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-    </Map>
-  );
+export default class MapView extends Component {
+  
+  //Coordinates of the beginning state
+  state = {
+      lat: 48.3705449,
+      lng: 10.89779,
+      zoom: 13,
+  }
+  render() {
+      return (
+        //this.props.incidents ?
+              <Map 
+                 center={[this.state.lat, this.state.lng]} 
+                 zoom={this.state.zoom} 
+                 style={{ width: '100%', height: '900px'}}
+              >
+              <TileLayer
+                attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+               />
+             </Map>
+               //:
+               //'Data is loading...'  
+        )
+   }
 }
