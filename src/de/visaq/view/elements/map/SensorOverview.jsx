2271ae42e407bb47a87670691c33a4af67117a89
Daniel Schild <daniel.schild@student.kit.edu>
2020-07-12 01:41:28 +0200

    Updates bootstrap dependency and expands sensoroverview

--- src/de/visaq/view/elements/map/SensorOverview.jsx
+++ src/de/visaq/view/elements/map/SensorOverview.jsx
@@ -9,6 +9,12 @@ import styles from './SensorOverview.module.css'
 import Card from 'react-bootstrap/Card'
 import Accordion from 'react-bootstrap/Accordion'
 import Button from 'react-bootstrap/Button'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Tooltip from 'react-bootstrap/Tooltip'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Figure from 'react-bootstrap/Figure'
+
+import testDia from '../../../../../testdiagram.png'
 
 class SensorOverview extends React.Component {
 
@@ -24,6 +30,8 @@ class SensorOverview extends React.Component {
     lng: 10.89779,
     zoom: 13,
   }
+
+
 
   render() {
     return (
@@ -43,34 +51,123 @@ class SensorOverview extends React.Component {
               Open
             </a>{" "}
           </p>
-          <Map 
-                 center={[this.state.lat, this.state.lng]} 
-                 zoom={this.state.zoom} 
-                 style={{ width: '100%', height: '900px'}}
-              >
-              <TileLayer
-                attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-               />
-             </Map>
+          <Map
+            center={[this.state.lat, this.state.lng]}
+            zoom={this.state.zoom}
+            style={{ width: '100%', height: '900px' }}
+          >
+            <TileLayer
+              attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </Map>
         </OffCanvasBody>
         <OffCanvasMenu className={styles.menuClass}>
+          <OverlayTrigger
+            placement="left"
+            delay={{ show: 250, hide: 400 }}
+            overlay={this.closeToolTip}
+          >
+            <p>
+              <a href="#" onClick={this.toggleOverview.bind(this)}>
+                <img src={closeX} alt="close" width='20px' />
+              </a>
+            </p>
+          </OverlayTrigger>
+          <h1>Sensor: [...]</h1>
           <p>
-            <a href="#" onClick={this.toggleOverview.bind(this)}>
-              <img src={closeX} alt="close" width='20px'/>
-            </a>
+            SensorTyp: [...]
           </p>
-          <div className = {styles.sensorname}>Sensorname: [Name]</div>
-          <ul>
-            <li>Luftdruck: 5000mPa</li>
-            <li>Lufttemperatur: 30 °C</li>
-            <li>Feinstaub: alles</li>
-            <li>Luftfeuchtigkeit: Ja</li>
-          </ul>
-          <div className={styles.diagram}>Diagram</div>
-
-
-          <p>SensorTyp: [Typ]</p>
+          <Accordion>
+            <Card>
+              <Card.Header>
+                <Accordion.Toggle as={Card.Header} eventKey="0">
+                  Luftdruck
+                </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>
+                  <p>Aktuell: 1000hPa</p>
+                  <Figure>
+                    <Figure.Image
+                      width={400}
+                      alt="171x180"
+                      src={testDia}
+                    />
+                    <Figure.Caption>
+                      Verlauf der Messwerte
+                  </Figure.Caption>
+                  </Figure>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Card.Header>
+                <Accordion.Toggle as={Card.Header} eventKey="1">
+                  Lufttemperatur
+                </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="1">
+                <Card.Body>
+                  <p>Aktuell: 30°C</p>
+                  <Figure>
+                    <Figure.Image
+                      width={400}
+                      alt="171x180"
+                      src={testDia}
+                    />
+                    <Figure.Caption>
+                      Verlauf der Messwerte
+                  </Figure.Caption>
+                  </Figure>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Card.Header>
+                <Accordion.Toggle as={Card.Header} eventKey="2">
+                  Luftfeuchtigkeit
+                </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="2">
+                <Card.Body>
+                  <p>Aktuell: 17%</p>
+                  <Figure>
+                    <Figure.Image
+                      width={400}
+                      alt="171x180"
+                      src={testDia}
+                    />
+                    <Figure.Caption>
+                      Verlauf der Messwerte
+                  </Figure.Caption>
+                  </Figure>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Card.Header>
+                <Accordion.Toggle as={Card.Header} eventKey="3">
+                  Feinstaub
+                </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="3">
+                <Card.Body>
+                  <p>Aktuell: 150ppm</p>
+                  <Figure>
+                    <Figure.Image
+                      width={400}
+                      alt="171x180"
+                      src={testDia}
+                    />
+                    <Figure.Caption>
+                      Verlauf der Messwerte
+                  </Figure.Caption>
+                  </Figure>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
         </OffCanvasMenu>
       </OffCanvas>
     );
@@ -79,6 +176,14 @@ class SensorOverview extends React.Component {
   toggleOverview() {
     // toggles the menu opened state
     this.setState({ isOverviewOpen: !this.state.isOverviewOpen });
+  }
+
+  closeToolTip(props) {
+    return (
+      <Tooltip id="button-tooltip" {...props}>
+        Hier klicken um die Detailansicht zu schließen
+      </Tooltip>
+    );
   }
 }
 
