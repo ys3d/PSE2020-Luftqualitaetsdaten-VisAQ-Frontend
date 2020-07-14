018f7851b5ee8eb845c1544c034f0ea984e01ec4
Daniel Schild <daniel.schild@student.kit.edu>
2020-07-14 18:58:18 +0200

    Sets showOverview to false as default

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { showOverview: true };
+    this.state = { showOverview: false };
 
     this.handleShowClick = this.handleShowClick.bind(this);
   }
