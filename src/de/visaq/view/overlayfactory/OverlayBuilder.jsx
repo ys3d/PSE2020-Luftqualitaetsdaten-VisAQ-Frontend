d4315361c12957730e5c6f31a4384f44ae266f04
Fabian Reinbold <fabian.reinbold@outlook.com>
2020-07-21 22:57:35 +0200

    Grid-loading + caching of things and observations

--- src/de/visaq/view/overlayfactory/OverlayBuilder.jsx
+++ src/de/visaq/view/overlayfactory/OverlayBuilder.jsx
@@ -15,10 +15,61 @@ export default class OverlayBuilder extends Component {
     }
 
     render() {
+        var southWest = this.props.mapState.bounds.getSouthWest();
+        var southCell = Math.floor(southWest.lat/this.props.gridSize);
+        var westCell = Math.floor(southWest.lng/this.props.gridSize);
+
+        var northEast = this.props.mapState.bounds.getNorthEast();
+        var northCell = Math.floor(northEast.lat/this.props.gridSize);
+        var eastCell = Math.floor(northEast.lng/this.props.gridSize);
+
+        var xCells = eastCell - westCell;
+        var yCells = northCell - southCell;
+
+        var cellsData = [];
+        var assocData = {};
+
+        if (yCells < 30 && xCells < 30) {
+            for (var y = 0; y <= yCells; y++) {
+                for (var x = 0; x <= xCells; x++) {
+                    var lat = (southCell + y) * this.props.gridSize;
+                    var lng = (westCell + x) * this.props.gridSize;
+                    if (this.props.mapState.cells.hasOwnProperty(`${lat}|${lng}`) || this.props.mapState.cells[`${lat}|${lng}`] != undefined) {
+                        cellsData.push(this.props.mapState.cells[`${lat}|${lng}`]);
+                    }
+                }
+            }
+        }
+
+        cellsData.forEach(function(cellData) {
+            if (cellData == null) {
+                return;
+            }
+
+            cellData.things.forEach(function(thing, index) {
+                assocData[thing.id] = [thing, cellData.observations[index]];
+            })
+        });
+
+        var data = [];
+
+        Object.keys(assocData).forEach(function(key) {
+            var thing = assocData[key][0];
+            var observation = assocData[key][1];
+
+            if (thing.locations === undefined) {
+                return;
+            } else if (observation == null) {
+                return;
+            }
+
+            data.push(assocData[key]);
+        });
+
         return (
             <div>
                 <Fragment>
-                    <SensorOverlayFactory things={this.props.mapState.things} observations={this.props.mapState.observations} airQ = {this.props.mapState.airQualityData} />
+                    <SensorOverlayFactory data = {data} airQ = {this.props.mapState.airQualityData} />
                 </Fragment>
             </div>
         );
