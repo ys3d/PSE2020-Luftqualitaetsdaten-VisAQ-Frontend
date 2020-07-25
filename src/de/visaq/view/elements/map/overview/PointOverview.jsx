893733fda8b4dd9c96fd74373a6a92669a14b144
Katharina Biernacka <kath.biernacka@gmail.com>
2020-07-25 19:56:15 +0200

    Remove old code

--- src/de/visaq/view/elements/map/overview/PointOverview.jsx
+++ src/de/visaq/view/elements/map/overview/PointOverview.jsx
@@ -19,42 +19,6 @@ class PointOverview extends Component {
     };
   }
 
-  getConsequencesAirQuality(data) {
-    if (data >= 1100) {
-      return 'overpressure'
-    } else if (data < 1000) {
-      return 'underpressure'
-    }
-    return 'normalPressure'
-  }
-
-  getConsequencesTemperature(data) {
-    if (data >= 37) {
-      return 'heat'
-    } else if (data < -5) {
-      return 'cold'
-    }
-    return 'normalTemperature'
-  }
-
-  getConsequencesHumidity(data) {
-    if (data >= 60) {
-      return 'wettness'
-    } else if (data < 30) {
-      return 'dry'
-    }
-    return 'normalHumidity'
-  }
-  
-  getConsequencesPM(data) {
-    if (data >= 1100) {
-      return 'overpressure'
-    } else if (data < 1000) {
-      return 'underpressure'
-    }
-    return 'normalPM'
-  }
-
   render() {
     const { t } = this.props;
 
@@ -70,7 +34,6 @@ class PointOverview extends Component {
             show={true}
             cardTitle={t('airPressure')} 
             currentValue="1000hPa"
-            consequences = {t(this.getConsequencesAirQuality(this.currentValue))}
             dataRowLabel="hPa"
             dataLabels={['January', 'February', 'March', 'April', 'May', 'June', 'July']}
             data={[65, 59, 80, 81, 56, 55, 40]}
@@ -80,7 +43,6 @@ class PointOverview extends Component {
             show={true}
             cardTitle={t('airTemperature')}
             currentValue="30°C"
-            consequences = {t(this.getConsequencesTemperature(this.currentValue))}
             dataRowLabel="°C"
             dataLabels={['January', 'February', 'March', 'April', 'May', 'June', 'July']}
             data={[65, 59, 80, 81, 56, 55, 40]}
@@ -90,7 +52,6 @@ class PointOverview extends Component {
             show={true}
             cardTitle={t('airHumidity')}
             currentValue="17%"
-            consequences = {t(this.getConsequencesHumidity(this.currentValue))}
             dataRowLabel="%"
             dataLabels={['January', 'February', 'March', 'April', 'May', 'June', 'July']}
             data={[65, 59, 80, 81, 56, 55, 40]}
@@ -100,7 +61,6 @@ class PointOverview extends Component {
             show={true}
             cardTitle={t('particulateMatter')}
             currentValue="155ppm"
-            consequences = {t(this.getConsequencesPM(this.currentValue))}
             dataRowLabel="ppm"
             dataLabels={['January', 'February', 'March', 'April', 'May', 'June', 'July']}
             data={[65, 59, 80, 81, 56, 55, 40]}
