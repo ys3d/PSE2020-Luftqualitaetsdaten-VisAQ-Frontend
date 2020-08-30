9770d706eb7a067804f1b2eb5700737f5280777f
Fabian Reinbold <fabian.reinbold@outlook.com>
2020-08-30 21:05:13 +0200

    Themed popover

--- src/de/visaq/view/HelpPopover.jsx
+++ src/de/visaq/view/HelpPopover.jsx
@@ -3,12 +3,13 @@ import { withTranslation } from 'react-i18next';
 import { MdHelpOutline } from 'react-icons/md';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Popover from 'react-bootstrap/Popover';
+import './HelpPopover.css'
 
 class HelpPopover extends Component {
     render() {
         return (
             <OverlayTrigger trigger={['focus', 'hover']} placement={this.props.placement} overlay={
-                <Popover id="popover-basic">
+                <Popover className="help-popover" id="popover-basic">
                     <Popover.Title as="h3">{this.props.title}</Popover.Title>
                     <Popover.Content>
                         {this.props.content}
